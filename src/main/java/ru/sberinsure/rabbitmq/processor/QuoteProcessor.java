19245d419bf713fa19351b7a2957f73b78d1c3a6
долбик дмитрий андреевич <dmitriy.dolbik@sberins.ru>
2024-12-06 11:33:19 +0300

    Add logs

--- src/main/java/ru/sberinsure/rabbitmq/processor/QuoteProcessor.java
+++ src/main/java/ru/sberinsure/rabbitmq/processor/QuoteProcessor.java
@@ -2,6 +2,7 @@ package ru.sberinsure.rabbitmq.processor;
 
 import io.smallrye.common.annotation.Blocking;
 import jakarta.enterprise.context.ApplicationScoped;
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
 import ru.sberinsure.rabbitmq.processor.model.Quote;
@@ -13,6 +14,7 @@ import java.util.Random;
  * The result is pushed to the "quotes" RabbitMQ exchange.
  */
 @ApplicationScoped
+@Slf4j
 public class QuoteProcessor {
 
     private final Random random = new Random();
@@ -22,6 +24,7 @@ public class QuoteProcessor {
     @Blocking
     public Quote precess(String quoteRequest) throws InterruptedException {
         // simulate some hard-working task
+        log.info("Receive quoteRequest: {}", quoteRequest);
         Thread.sleep(1000);
         return new Quote(quoteRequest, random.nextInt(100));
     }
